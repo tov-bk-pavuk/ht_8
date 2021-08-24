8479ad501aaa3a87e0c08e47ebb55e57f30f0f48
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-08-24 23:57:52 +0300

    not yet: validation, object creations, redirect

--- ht_8/people/models.py
+++ ht_8/people/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(verbose_name='Имя', max_length=50)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=50)
+    email = models.EmailField(verbose_name='Эл. почта')
--- ht_8/people/views.py
+++ ht_8/people/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import PersonForm
+
+
+def people(request):
+    form = PersonForm()
+    data = {'form': form}
+    return render(request, 'people.html', context=data)
